7b8251c31bc508893f7249f0c2c8b8404dc0473b
Piyada Thongtem <45333950+walkergameboy@users.noreply.github.com>
2021-01-08 04:56:53 +0700

    herosection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,18 @@ import React from 'react'
 function HeroSection() {
     return (
         <div className='hero-container'>
-            
+            <video src="/video/video-2.mp4" autoPlay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className="hero-btns">
+                <button
+                className='btns'
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'
+                >
+                    GET START
+                </button>
+            </div>
         </div>
     )
 }
